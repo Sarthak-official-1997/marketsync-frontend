9e63ce2c59c648c350db82749ae0f80bb438574f
Sarthak 915 <sarthaksharma1997@gmail.com>
2026-08-08 19:01:32 +0530

    removed settings icon from desktop to name menu list

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-53feb3a3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bd8tgbf7an4"
+    "revision": "0.crh936798v4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
